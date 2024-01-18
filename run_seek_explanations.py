c3aa462b33117ef1a63070c609a08d4fc94df21c
Filipe Paulino <filipe.jose.paulino@gmail.com>
2024-01-18 18:24:04 +0000

    new train_embeddings.py and other minor changes

--- run_seek_explanations.py
+++ run_seek_explanations.py
@@ -477,8 +477,10 @@ if __name__== '__main__':
     ####################################### PPI prediction
 
     path_file_representation = "./PPI/Embeddings/Emb_pair_maxdepth4_nwalks100_Avg_disjointcommonancestor.txt"
-    path_file_model = "./PPI/Models/RF/Model_RF.pickle"
+    # path_file_model = "./PPI/Models/RF/Model_RF.pickle"
+    path_file_model = "./PPI/Models/"
     alg = "RF"
+    # alg = "MLP"
     path_graph = "./PPI/KG.gpickle"
     path_label_classes = "PPI/Labelclasses.pkl"
     path_explanations = "./PPI/Explanations/"
